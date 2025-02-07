faea451c3aaf6f66b886068b479a508ced85d2de
Brodie <brodieman500@gmail.com>
2025-02-06 20:20:48 -0600

    Added functional like and partially functional replies

--- Chirper/Chirper_app/models.py
+++ Chirper/Chirper_app/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.utils.timezone import now
+import uuid
 
 # Create your models here.
 
 class Chirp(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+    like_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.content[:50]
-    
- 
+
+class Reply(models.Model):
+    chirp = models.ForeignKey(Chirp, on_delete=models.CASCADE, related_name='replies')
+    content = models.TextField(max_length=255)
+    created_at = models.DateField(auto_now_add=True)
--- Chirper/Chirper_app/urls.py
+++ Chirper/Chirper_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('account', views.account, name="account"),
-    path('create_chirp/', views.create_chirp, name="create_chirp")
+    path('create_chirp/', views.create_chirp, name="create_chirp"),
+    path('reply/<uuid:chirp_id>/', views.create_reply, name="create_reply"),
+    path('like_chirp/<uuid:chirp_id>/', views.like_chirp, name="like_chirp"),
 ]
