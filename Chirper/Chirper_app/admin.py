24bfb0ac263d6ac9da5b85215345cb5d334c6fee
Brodie <brodieman500@gmail.com>
2025-02-06 20:20:48 -0600

    Added functional like and partially functional replies

--- Chirper/Chirper_app/admin.py
+++ Chirper/Chirper_app/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Chirp
+from .models import Chirp, Reply
 
 @admin.register(Chirp)
 class ChirpAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'created_at')
-    search_fields = ('title',) 
-    list_filter = ('created_at',)
+    list_display = ('title', 'content', 'created_at', 'id')
+    search_fields = ('title', 'created_at', 'id') 
+    list_filter = ('created_at', 'id')
+
+@admin.register(Reply)
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = ('content', 'created_at')
+    search_fields = ('content', 'created_at')
+    list_filter = ('content', 'created_at')
