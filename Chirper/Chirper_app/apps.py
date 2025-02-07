ad0a2ee16ca5f04bc0af91b7f9cb543afd6aec1a
Marcus Sweet <marcussweet0211@gmail.com>
2025-02-06 19:28:54 -0600

    made base model for user

--- Chirper/Chirper_app/apps.py
+++ Chirper/Chirper_app/apps.py
@@ -4,3 +4,9 @@ from django.contrib import admin
 class ChirperAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Chirper_app'
+
+
+class RegisterUser(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.set_password(obj.password)
+        obj.save()
--- Chirper/Chirper_app/models.py
+++ Chirper/Chirper_app/models.py
@@ -11,4 +11,12 @@ class Chirp(models.Model):
     def __str__(self):
         return self.content[:50]
     
- 
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.username
