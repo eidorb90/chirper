c8ad1fd066d1b2ce9c562c984546983bb4954ca5
Marcus Sweet <marcussweet0211@gmail.com>
2025-02-11 20:56:52 -0600

    should be good

--- Chirper/Chirper_app/forms.py
+++ Chirper/Chirper_app/forms.py
@@ -4,14 +4,18 @@ from .models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "first_name", "last_name", "email", "password1", "password2")
 
     def save(self, commit=True):
         user = super().save(commit=False)
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
         user.email = self.cleaned_data["email"]
         if commit:
             user.save()
@@ -33,3 +37,4 @@ class EmailAuthenticationForm(AuthenticationForm):
         ),
         label="Password",
     )
+    
