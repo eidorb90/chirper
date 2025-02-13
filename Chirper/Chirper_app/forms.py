bfe0073349585e9c443938586470a5f8f3340a59
Marcus Sweet <marcussweet0211@gmail.com>
2025-02-13 10:46:17 -0600

    input box

--- Chirper/Chirper_app/forms.py
+++ Chirper/Chirper_app/forms.py
@@ -32,8 +32,9 @@ class CustomUserCreationForm(UserCreationForm):
     )
     password2 = forms.CharField(
         widget=forms.PasswordInput(attrs={
-            'class': 'border-2 border-gray-300 rounded-md p-2 w-full h-12 text-white',
-            'style': 'height: 2.5rem; font-size: 90%; background-color: gray; border-color: gray;'
+            'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+            'style': 'height: 2.5rem; font-size: 90%; background-color: gray; border-color: gray;',
+            'placeholder': 'Confirm Password'
         })
     )
 
