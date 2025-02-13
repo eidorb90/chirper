4ac6f6a1411269cc07b0ac1eecd97a85e8fa3d9d
Marcus Sweet <marcussweet0211@gmail.com>
2025-02-13 11:04:20 -0600

    white

--- Chirper/Chirper_app/forms.py
+++ Chirper/Chirper_app/forms.py
@@ -6,7 +6,7 @@ class CustomUserCreationForm(UserCreationForm):
     first_name = forms.CharField(
         required=True,
         widget=forms.TextInput(attrs={
-            'class': 'bg-gray-700 border border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5',
+            'class': 'bg-gray-700 border border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-white',
             'style': 'height: 2.5rem; font-size: 90%; background-color: gray; border-color: gray;',
             'placeholder': 'First Name'
         })
@@ -14,7 +14,7 @@ class CustomUserCreationForm(UserCreationForm):
     last_name = forms.CharField(
         required=True,
         widget=forms.TextInput(attrs={
-            'class': 'bg-gray-700 border border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5',
+            'class': 'bg-gray-700 border border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-white',
             'style': 'height: 2.5rem; font-size: 90%; background-color: gray; border-color: gray;',
             'placeholder': 'Last Name'
         })
@@ -22,21 +22,21 @@ class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(
         required=True,
         widget=forms.EmailInput(attrs={
-            'class': 'bg-gray-700 border border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5',
+            'class': 'bg-gray-700 border border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-white',
             'style': 'height: 2.5rem; font-size: 90%; background-color: gray; border-color: gray;',
             'placeholder': 'Email'
         })
     )
     password1 = forms.CharField(
         widget=forms.PasswordInput(attrs={
-            'class': 'bg-gray-700 border border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5',
+            'class': 'bg-gray-700 border border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-white',
             'style': 'height: 2.5rem; font-size: 90%; background-color: gray; border-color: gray;',
             'placeholder': 'Password'
         })
     )
     password2 = forms.CharField(
         widget=forms.PasswordInput(attrs={
-            'class': 'bg-gray-700 border border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5',
+            'class': 'bg-gray-700 border border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-white',
             'style': 'height: 2.5rem; font-size: 90%; background-color: gray; border-color: gray;',
             'placeholder': 'Confirm Password'
         })
