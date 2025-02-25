b5aded7a0851e53a5f4dbc042717b1078575333c
Abe Gomez <abraham.gomez@cune.org>
2025-02-24 22:18:33 -0600

    Rich Text Editor Enabled, Still Needs a Little Work

--- Chirper/Chirper_app/migrations/0001_initial.py
+++ Chirper/Chirper_app/migrations/0001_initial.py
@@ -29,6 +29,7 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.CreateModel(
+<<<<<<< HEAD
             name='UserFollowing',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
@@ -36,6 +37,8 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.CreateModel(
+=======
+>>>>>>> ac5c8c0 (Rich Text Editor Enabled, Still Needs a Little Work)
             name='User',
             fields=[
                 ('password', models.CharField(max_length=128, verbose_name='password')),
@@ -50,10 +53,13 @@ class Migration(migrations.Migration):
                 ('email', models.EmailField(max_length=254, unique=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('username', models.CharField(max_length=50, unique=True)),
+<<<<<<< HEAD
 
                 ('private', models.BooleanField(default=False)),
                 ('profile_image', models.ImageField(blank=True, default='profile_pic/default.png', null=True, upload_to='profile_pic/', validators=[django.core.validators.FileExtensionValidator(['png', 'jpg', 'jpeg', 'gif'])])),
                 ('followers', models.ManyToManyField(related_name='following', through='Chirper_app.UserFollowing', to=settings.AUTH_USER_MODEL)),
+=======
+>>>>>>> ac5c8c0 (Rich Text Editor Enabled, Still Needs a Little Work)
                 ('profile_image', models.ImageField(blank=True, default='profile_pic/default.png', null=True, upload_to='profile_pic/', validators=[django.core.validators.FileExtensionValidator(['png', 'jpg', 'jpeg', 'gif'])])),
                 ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
                 ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
@@ -67,6 +73,7 @@ class Migration(migrations.Migration):
                 ('objects', django.contrib.auth.models.UserManager()),
             ],
         ),
+<<<<<<< HEAD
         migrations.AddField(
             model_name='userfollowing',
             name='following_user',
@@ -77,6 +84,8 @@ class Migration(migrations.Migration):
             name='user',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='following_relationships', to=settings.AUTH_USER_MODEL),
         ),
+=======
+>>>>>>> ac5c8c0 (Rich Text Editor Enabled, Still Needs a Little Work)
         migrations.CreateModel(
             name='Reply',
             fields=[
@@ -101,8 +110,11 @@ class Migration(migrations.Migration):
             name='likes',
             field=models.ManyToManyField(blank=True, related_name='liked_chirps', to=settings.AUTH_USER_MODEL),
         ),
+<<<<<<< HEAD
         migrations.AlterUniqueTogether(
             name='userfollowing',
             unique_together={('user', 'following_user')},
         ),
+=======
+>>>>>>> ac5c8c0 (Rich Text Editor Enabled, Still Needs a Little Work)
     ]
